ed0008518868f0424f96e0b5581aaebaa56d2376
Dennis Leon <dennis.leon39@gmail.com>
2014-07-19 15:38:00 +1000

    Add iterm customised settings file

--- manifests/dev.pp
+++ manifests/dev.pp
@@ -11,4 +11,20 @@ class iterm2::dev($version = '20140421') {
     source   => "http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_${version}.zip",
     provider => 'compressed_app'
   }
+
+  file { "plist":
+    path    => "/Users/${::boxen_user}/Library/Preferences/com.googlecode.iterm2.plist",
+    source  => 'puppet:///modules/iterm2/com.googlecode.iterm2.plist',
+    mode    => '0755',
+    force   => true,
+    require => Package['iTerm']
+  }
+
+  exec { "sync-iterm-settings":
+    command => "defaults read com.googlecode.iterm2",
+    path    => "/usr/local/bin/:/bin/",
+    require => File['plist']
+}
+
+
 }
